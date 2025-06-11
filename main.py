2d61face05c86381c08edd06b059ffe1a62f5755
Sakthivel <128593420+shakthi2230@users.noreply.github.com>
2025-06-11 17:42:52 +0530

    Updated socket issues in  main.py

--- main.py
+++ main.py
@@ -53,4 +53,5 @@ def handle_disconnect():
     emit('user-left', request.sid, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
+
